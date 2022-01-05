96b64b4b0fcd57c63d59f0ac5e67850e322f7941
Samin Amanat <amanat361@gmail.com>
2022-01-05 00:00:59 -0800

    updated device motion once again (likely working now)

--- PWA/app/device-motion.js
+++ PWA/app/device-motion.js
@@ -1,25 +1,10 @@
-const el = document.getElementById('motion');
+const rotation = document.getElementById('rotation');
 
-function askPermission() {
-  // feature detect
-  el.innerText = "i hate my life"
-  if (typeof DeviceOrientationEvent.requestPermission === "function") {
-    DeviceOrientationEvent.requestPermission()
-      .then(permissionState => {
-        if (permissionState === "granted") {
-          el.innerText = "meep moop"
-          window.addEventListener('devicemotion', (event) => {
-              el.innerText = (Math.round((event.acceleration.x + Number.EPSILON) * 100) / 100) + ' m/s2';
-          });
-        }
-      })
-      .catch(console.error);
-  } else {
-    window.addEventListener('devicemotion', (event) => {
-        el.innerText = "i hate this website"
-        el.innerText = (Math.round((event.acceleration.x + Number.EPSILON) * 100) / 100) + ' m/s2';
-    });
-  }
+function handleOrientation(event) {
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
+  rotation.innerText = `Alpha: ${alpha}\n\nBeta: ${beta}\n\nGamma: ${gamma}`;
 }
 
-askPermission();
+window.addEventListener("deviceorientation", handleOrientation, true);
